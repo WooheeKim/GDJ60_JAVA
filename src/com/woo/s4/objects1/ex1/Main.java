2ecc6b9eee61fd0d9306a12bf8998ca792421b06
Kimwoohee <kimdngml12@gmail.com>
2023-01-03 14:18:20 +0900

    1

--- src/com/woo/s4/objects1/ex1/Main.java
+++ src/com/woo/s4/objects1/ex1/Main.java
@@ -4,6 +4,7 @@ package com.woo.s4.objects1.ex1;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("start");
 		MenuController mc = new MenuController();
 		
 		mc.start();
