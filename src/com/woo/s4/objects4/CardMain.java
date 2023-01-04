9b68726f498d09c9a895582d8c4f08dd05a5a4ea
Kimwoohee <kimdngml12@gmail.com>
2023-01-04 11:48:59 +0900

    1

--- src/com/woo/s4/objects4/CardMain.java
+++ src/com/woo/s4/objects4/CardMain.java
@@ -6,19 +6,19 @@ public class CardMain {
 		// 클래스명.변수명
 		System.out.println(Card.size); 
 		
-//		Card card = new Card();
-//		card.setShape("Heart");
-//		card.setColor("Red");
-//		card.setNumber(1);
-//		
-//		Card card2 = new Card();
-//		card2.setShape("Spade");
-//		card2.setColor("Black");
-//		card2.setNumber(12);
-//		card2.size=9;
-//		
-//		card.info();
-//		card2.info();
+		Card card = new Card();
+		card.setShape("Heart");
+		card.setColor("Red");
+		card.setNumber(1);
+		
+		Card card2 = new Card();
+		card2.setShape("Spade");
+		card2.setColor("Black");
+		card2.setNumber(12);
+		card2.size=9;
+		
+		card.info();
+		card2.info();
 		
 		
 
