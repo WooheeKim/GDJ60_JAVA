ef811ecb35838b6da59f8ba5fe8a857f2a4d6295
Kimwoohee <kimdngml12@gmail.com>
2023-01-05 10:53:48 +0900

    AddRobot

--- src/com/woo/s4/objects6/Main.java
+++ src/com/woo/s4/objects6/Main.java
@@ -15,6 +15,11 @@ public class Main {
 		
 		robot = null;
 		
+		System.out.println("Add Robot에서 작업");
+		
+		robot = new Robot();
+		
+		System.out.println(robot);
 		
 		System.out.println("프로그램 종료");
 	}
